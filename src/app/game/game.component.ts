18bb4b2137f6776035d889a89d34afad473526b0
mattispaulin <mattis.paulin@free.fr>
2020-01-20 20:15:44 +0100

    3 files modified

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -10,9 +10,9 @@ export class GameComponent implements OnInit {
   sante = 0;
   motivation=0;
   current = 0;
-  scenes = [];
+  scenes = data_scenes;
+  
   constructor() {
-    this.scenes = JSON.parse()
 
   }
 
